f0b9b23db5f0b9f55bb52b50d2e9c299274ebb5d
“CollinABullock” <cbullock@schoolofrock.com>
2023-10-09 22:52:57 -0500

    adding image

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function Home() {
     <h1>SimpsonsPedia</h1>
       <h4>A guide to characters, products, and episodes</h4>
       <NavBar />
+      <img src="https://i.pinimg.com/736x/b8/da/6e/b8da6e8716ac3b784a22d34e88648340.jpg" alt="The Simpsons" />
       <p>(c) 2023, Collin A. Bullock</p>
       </>
   )
